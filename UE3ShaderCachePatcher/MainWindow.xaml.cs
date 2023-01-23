4d319a37e303397a94257be9b7cdf11455a1427c
Tuomo <tuokri@tuta.io>
2023-01-23 23:09:22 +0200

    Basic GUI

--- UE3ShaderCachePatcher/MainWindow.xaml.cs
+++ UE3ShaderCachePatcher/MainWindow.xaml.cs
@@ -24,5 +24,41 @@ namespace UE3ShaderCachePatcher
         {
             InitializeComponent();
         }
+
+        private void ButtonSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            // dialog.FileName = "Document"; // Default file name
+            dialog.DefaultExt = ".u"; // Default file extension
+            dialog.Filter = "UE3 script packages (.u)|*.u"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                lstShaderCacheObjects.Items.Clear();
+                lstTargetObjects.Items.Clear();
+
+                fileNameBlock.Text = dialog.FileName;
+
+                lstShaderCacheObjects.Items.Add("SeekFreeShaderCache");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+                lstShaderCacheObjects.Items.Add("asd");
+
+                lstTargetObjects.Items.Add("someTargetObject");
+            }
+        }
     }
 }
